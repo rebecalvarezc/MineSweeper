7766540910ac5e5a330628884342ee9e4bda933b
rebecalvarezc <realce@alumni.uv.es>
2021-10-06 08:54:23 +0200

    added some comments on play

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from board import Minesweeper
 from global_functions import movement_menu
 
-game = Minesweeper(5, 5)
+game = Minesweeper()  # add here (rows, columns) to change the size f the board.
 game.play(movement_menu)
